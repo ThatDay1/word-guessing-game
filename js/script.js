24da1a4d3496546e7ff4d9661b1123fed9e3a1cf
Ogun Karaoglu <ogunkaraoglu62@gmail.com>
2023-11-09 21:26:26 +0300

    Here Have Code
    completing javascript and shortin css

--- js/script.js
+++ js/script.js
@@ -5,20 +5,79 @@ const inputs = document.querySelector(".inputs"),
     wrongLetter = document.querySelector(".wrong-letter span"),
     answerInput = document.querySelector(".answer-input");
 
+let words;
+let incorrects = [];
+let corrects = [];
+let maxGuesses;
+
 const randomWord = () => {
     let randObj = wordList[Math.floor(Math.random() * wordList.length)];
     //console.log(randObj);
-    let words = randObj.word;
+    words = randObj.word;
+    maxGuesses = 5;
+    corrects = [];
+    incorrects = [];
+
+
     hint.innerText = randObj.hint;
+    guessLeft.innerText = maxGuesses;
+    wrongLetter.innerHTML = incorrects;
     console.log(words);
 
     let html = "";
-
+    
     for (let i = 0; i < words.length; i++) {
         html += `<input type="text" disabled>`;
     }
+
     inputs.innerHTML = html;
 }
+
 randomWord()
 
-resetBtn.addEventListener("click", randomWord);
+const initGame = e => {
+    let key = e.target.value;
+    //console.log(e);
+
+    if (key.match(/^[A-Za-z]+$/) && !incorrects.includes(key) && !corrects.includes(key)) {
+        //console.log(key);
+
+        if (words.includes(key)) {
+            //console.log("harf bulundu");
+
+            for (let i = 0; i < words.length; i++) {
+                if (words[i] === key) {
+                    corrects.push(key);
+                    inputs.querySelectorAll("input")[i].value = key;
+                }
+            }
+        }
+        else {
+            //console.log("harf bulunamadi");
+            maxGuesses--;
+            incorrects.push(`${key}`);
+            //console.log(incorrects);
+        }
+        guessLeft.innerText = maxGuesses;
+        wrongLetter.innerText = incorrects;
+    }
+    answerInput.value = "";
+
+    setTimeout(() => {
+        if (corrects.length === words.length) {
+            alert(`Good Job! You found the correct answer. Answer is: ${words.toUpperCase()}`);
+            randomWord()
+        }
+        else if (maxGuesses < 1) {
+            alert("Game Over!");
+    
+            for (let i = 0; i < words.length; i++) {
+                inputs.querySelectorAll("input")[i].value = words[i];
+            }
+        }
+    }, 200)
+}
+
+resetBtn.addEventListener("click", randomWord)
+answerInput.addEventListener("input", initGame)
+document.addEventListener("keydown", () => answerInput.focus())
